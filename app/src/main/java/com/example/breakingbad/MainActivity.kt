71519b5e6e644860c3a1c2a38a15fbfc40788db8
GiorgiKakhetelidze <kakhetelidzegiorgi@yahoo.com>
2021-02-23 17:33:11 +0400

    BB_Second_Ver

--- app/src/main/java/com/example/breakingbad/MainActivity.kt
+++ app/src/main/java/com/example/breakingbad/MainActivity.kt
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    companion object{
-        const val SPLASH_TIME_OUT = 4000
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
         supportActionBar?.hide()
+        window.setBackgroundDrawable(null)
     }
 }
